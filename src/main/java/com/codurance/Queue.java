12efbd7ad18bd301edce2b20541d0df74e5451b2
Tom <thomas.spencer@codurance.com>
2022-03-13 15:29:45 +0000

    Add size == 0 to isEmpty()

--- src/main/java/com/codurance/Queue.java
+++ src/main/java/com/codurance/Queue.java
@@ -2,30 +2,31 @@ package com.codurance;
 
 public class Queue {
 
-    private boolean isEmpty = true;
     private int number;
     private int size;
 
     public boolean isEmpty() {
-        return isEmpty;
+        return size == 0;
     }
 
     public void add(int element) {
         size++;
         number = element;
-        isEmpty = false;
     }
 
     public int remove() {
-        if (size < 1) {
-            throw new UnderflowException();
-        }
+        requireNotEmpty();
         --size;
-        isEmpty = true;
         return number;
     }
 
     public int size() {
         return size;
+    }
+
+    private void requireNotEmpty() {
+        if (size < 1) {
+            throw new UnderflowException();
+        }
     }
 }
