28c21ec016a5d64562c1e5784a1401364eb10546
Tom <thomas.spencer@codurance.com>
2022-03-13 15:21:50 +0000

    Add size()

--- src/main/java/com/codurance/Queue.java
+++ src/main/java/com/codurance/Queue.java
@@ -3,12 +3,26 @@ package com.codurance;
 public class Queue {
 
     private boolean isEmpty = true;
+    private int number;
+    private int size;
 
     public boolean isEmpty() {
         return isEmpty;
     }
 
     public void add(int element) {
+        size++;
+        number = element;
         isEmpty = false;
+    }
+
+    public int remove() {
+        --size;
+        isEmpty = true;
+        return number;
+    }
+
+    public int size() {
+        return size;
     }
 }
--- src/test/java/com/codurance/QueueTest.java
+++ src/test/java/com/codurance/QueueTest.java
@@ -18,7 +18,46 @@ public class QueueTest {
         queue.add(7);
         assertThat(queue.isEmpty())
                 .isFalse();
+        assertThat(queue.size())
+                .isEqualTo(1);
     }
 
+    @Test
+    void emptyIsTrueAfterAddThenRemove() {
+        Queue queue = new Queue();
+        queue.add(7);
+        queue.remove();
+        assertThat(queue.isEmpty())
+                .isTrue();
+        assertThat(queue.size())
+                .isZero();
+    }
 
+    @Test
+    void add7ThenRemoveReturns7() {
+        Queue queue = new Queue();
+        queue.add(7);
+        int element = queue.remove();
+        assertThat(element)
+                .isEqualTo(7);
+    }
+
+    @Test
+    void add4ThenRemoveReturns4() {
+        Queue queue = new Queue();
+        queue.add(4);
+        int element = queue.remove();
+        assertThat(element)
+                .isEqualTo(4);
+    }
+
+    @Test
+    void addTwoNumbersReturnsSizeOf2() {
+        Queue queue = new Queue();
+        queue.add(4);
+        queue.add(7);
+
+        assertThat(queue.size())
+                .isEqualTo(2);
+    }
 }
